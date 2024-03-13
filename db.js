d0c5eb6ce81f96cf1d7278b879bb1650acf8bcb5
aadrshdubey1 <aadrshdubey181@gmail.com>
2024-03-13 22:20:49 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const express = require('express')
 require('dotenv').config();
 
 
-const mongoURI ='mongodb://127.0.0.1:27017/hotel'
+// const mongoURI ='mongodb://127.0.0.1:27017/hotel'
+const mongoURI = "mongodb+srv://AadrshDubey:aadrsh123@cluster0.qqpd4zc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 const connectToMongo=async()=>{
